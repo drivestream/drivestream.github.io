cb778849f21a60de46b6c07b2c13e396cdf8bb98
Vision <ubuntugrive@gmail.com>
2020-04-14 12:38:43 +0700

    update

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ function updatePlayer() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(this.responseText);
             document.querySelector(
                 '#stream-tester-player'
-            ).innerHTML = `<iframe src="${this.responseText}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+            ).innerHTML = `<iframe src="${response.proxyPlayer}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
 
             document.querySelector('#api-code').innerText = `https://getplayer.thundersave.com/?u=demo&id=${document.querySelector('input').value}`;
 
-            document.querySelector('#embed-code').innerText = `<iframe src="${this.responseText}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+            document.querySelector('#embed-code').innerText = `<iframe src="${response.proxyPlayer}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
         }
     };
     xhttp.open('GET', `https://getplayer.thundersave.com/?u=demo&id=${document.querySelector('input').value}`, true);
