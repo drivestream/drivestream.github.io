e6e4c39bae04745efe714f64a9438d3fb1e8e870
Vision <ubuntugrive@gmail.com>
2020-05-11 09:30:54 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.querySelector('#api-url').innerText = 'https://getplayer.thundersave.com/?u=demo&id=<your_drive_link>';
+document.querySelector('#api-url').innerText = 'https://tool.thundersave.com/getplayer?u=demo&id=<your_drive_link>';
 function updatePlayer() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -6,14 +6,14 @@ function updatePlayer() {
             let response = JSON.parse(this.responseText);
             document.querySelector(
                 '#stream-tester-player'
-            ).innerHTML = `<iframe src="${response.proxyPlayer}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+            ).innerHTML = `<iframe src="${response.player}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
 
-            document.querySelector('#api-code').innerText = `https://getplayer.thundersave.com/?u=demo&id=${document.querySelector('input').value}`;
+            document.querySelector('#api-code').innerText = `https://tool.thundersave.com/getplayer?u=demo&id=${document.querySelector('input').value}`;
 
-            document.querySelector('#embed-code').innerText = `<iframe src="${response.proxyPlayer}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+            document.querySelector('#embed-code').innerText = `<iframe src="${response.player}"  frameborder="0" scrolling="no" allowfullscreen></iframe>`;
         }
     };
-    xhttp.open('GET', `https://getplayer.thundersave.com/?u=demo&id=${document.querySelector('input').value}`, true);
+    xhttp.open('GET', `https://tool.thundersave.com/getplayer?u=demo&id=${document.querySelector('input').value}`, true);
     xhttp.send();
 }
 document.querySelector('#teststream-btn').addEventListener('click', function() {
